8beafdf7850cbc9f033c9fffd454a1c13c9373c9
Donghoon Kang <d159123@naver.com>
2023-12-16 23:56:21 +0900

    first comit

---
+++ types/1-1-basic.ts
@@ -0,0 +1,27 @@
+{
+  /**
+   * Javascript
+   * Primitive: numbaer, string, boolean, bigint, symbol, null, undefined
+   * Object: function, array, ...
+   */
+
+  //number
+  const num:number = 4;
+
+  //string
+  const str:string = "hello";
+
+  //boolean
+  const boa:boolean = true;
+
+  //undefined
+  let name:undefined; // undefined 선언 X
+  let age : number | undefined
+
+  age= undefined;
+  age= 3;
+
+  //null
+  let person: null; // null 선언 x
+  let person2: string | null;
+}
