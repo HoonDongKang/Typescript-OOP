08ef5c4ef200f4de311183d6e76573bb6c343357
Donghoon Kang <d159123@naver.com>
2024-02-04 01:13:15 +0900

    interface

--- advanced/9-1-interface.ts
+++ advanced/9-1-interface.ts
@@ -6,3 +6,43 @@ interface PositionInterface {
   x: number;
   y: number;
 }
+
+// obj
+const obj1: PositionType = {
+  x: 1,
+  y: 1
+}
+// const obj2: PositionInterface = {
+//   x: 1,
+//   y: 1
+// }
+
+// class
+class Pos1 implements PositionType{
+  x: number;
+  y: number;
+}
+// class Pos2 implements PositionInterface{
+//   x: number;
+//   y: number;
+// }
+
+interface ZpositionInterface extends PositionInterface{
+  z:number
+}
+
+type ZPositionType = PositionType & { z: number };
+
+//only
+
+interface PositionInterface{
+  z: number
+}
+
+type Person = {
+  name: string,
+  age: Number
+}
+
+type Name = Person['name']
+
